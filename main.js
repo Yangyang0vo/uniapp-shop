7f0cc2625e14a15878de249fa9e822c4909583e4
洋洋 <2372542263@qq.com>
2022-07-22 16:07:39 +0800

    完成了home首页的开发

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+// 导入网络请求包
+import {
+  $http
+} from '@escook/request-miniprogram'
+//挂载到顶级对象  
+uni.$http = $http
+// 请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+// 响应拦截器
+$http.afterRequest = function(options) {
+  uni.hideLoading()
+}
+// 封装的展示消息提示的方法
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
