e75ef4b6165b00fdb9b315ce026ee9f93abdf0e2
洋洋 <2372542263@qq.com>
2022-07-23 14:56:01 +0800

    完成了搜索功能的开发

---
+++ components/my-search/my-search.vue
@@ -0,0 +1,62 @@
+<template>
+  <view class="my-search-container" :style="{'background-color':bgColor}" @click="searchBoxHandler">
+    <view class="my-search-box" :style="{'border-radius':radius + 'px'}">
+      <uni-icons type="search" size="17"></uni-icons>
+      <text class="placeholder">搜索</text>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: "my-search",
+    props: {
+      // 背景颜色
+      bgColor: {
+        type: String,
+        default: '#c00000'
+      },
+      //原角尺寸 
+      radius: {
+        type: Number,
+        default: 17
+      }
+    },
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+      searchBoxHandler() {
+        this.$emit('click')
+      }
+    }
+
+  }
+</script>
+
+<style lang="scss">
+  .my-search-container {
+    height: 50px;
+    // background-color: #C00000;
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+
+    .my-search-box {
+      height: 36px;
+      background-color: #ffffff;
+      // border-radius: 15px;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .placeholder {
+        font-size: 15px;
+        margin-left: 5px;
+      }
+    }
+  }
+</style>
--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,5 +1,8 @@
 <template>
   <view>
+    <!-- 使用自定义搜索组件 -->
+    <!-- <my-search :bgColor="'skyblue'" :radius="15"></my-search> -->
+    <my-search @click="gotoSearch"></my-search>
     <view class="scroll-view-container">
       <!-- 左侧的滚动视图区域 -->
       <scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
@@ -51,7 +54,8 @@
       // 获取当前系统的信息
       const sysInfo = uni.getSystemInfoSync()
       // 为 wh 窗口可用高度动态赋值
-      this.wh = sysInfo.windowHeight
+      // 可用高度 = 屏幕高度 - navigationBar高度 - tabBar高度 - 自定义的search组件高度
+      this.wh = sysInfo.windowHeight - 50
       // 调用获取分类列表数据的方法
       this.getCateList()
     },
@@ -81,6 +85,11 @@
       gotoGoodsList(item) {
         uni.navigateTo({
           url: '/subpkg/goods_list/goods_list?cid=' + item.cat_id
+        })
+      },
+      gotoSearch() {
+        uni.navigateTo({
+          url: '/subpkg/search/search'
         })
       }
     }
